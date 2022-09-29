127877201cb2155454c917c2509e53e301d81a74
Sanssolvant <113373512+Sanssolvant@users.noreply.github.com>
2022-09-29 21:49:54 +0200

    Add files via upload

---
+++ Website/languages/en.php
@@ -0,0 +1,60 @@
+<?php
+    $lang = array(
+        /* index webseite */
+        "Home" => "Home",
+        "Contact" => "Contact",
+        "About me" => "About me",
+        "my Projects" => "my Projects",
+        "hi" => "Hello and ",
+        "welcome" => "Welcome ",
+        "glad" => "I'm glad you found my site :)",
+        "de" => "DE-Deutsch",
+        "en" => "EN-English",
+
+        /* kontakt webseite */
+        "alert-success" => "Message Sent! Thank you for contacting me.",
+        "alert-error" => "Something went wrong! Please try again.",
+        "contact-form" => "Contact Me",
+        "Your Name" => "Name",
+        "Your Email" => "Email",
+        "Subject" => "Subject",
+        "Message" => "Message",
+        "Send" => "Send",
+
+        /* meine Projekte webseite */
+        "viergewinnt-text" => "The classic Connect Four game.",
+        "Players" => "(2 Players)",
+        "Download" => "Download",
+        "bankomat-text" => "A mix between an ATM and e-banking.",
+        "MySql" => "(Connected with a MySql database)",
+        "umrechner-text" => "A converter from/to Hexidecimal, Binary, and Decimal.",
+        "zeitmesser-text" => "Measures how long a program has been open.",
+        "open-already" => "(Must be open already)",
+        "html-text" => "The website you are currently on was created by myself,",
+        "standard-languages" => "using the standard languages",
+        "html" => "HTML/CSS/Javascript/PHP.",
+        "viergewinnt-title" => "Connect Four",
+        "bankomat-title" => "ATM",
+        "umrechner-title" => "Converter",
+        "zeitmesser-title" => "Timer",
+        "webseite-title" => "Website",
+
+        /* Übermich Webseite  */
+        "hi me" => "That's me...",
+        "übermich-text" => "Born and raised in Uznach, Switzerland, I quickly found interest in the world of computer science. From a young age I was fascinated by this rectangular thing called computer, at the age of 18 I configured and assembled my first one.
+        Over the years, my attention turned more and more towards programming, so much that at the end of 2022 I started a shortened second apprenticeship for 2 years as a computer scientist specializing in application development in Zürich.",
+        "übermich-text2" => "At the moment I haven't really specialized yet in any IT area or programming language. But, I have gained most experience in the areas of Java and website programming. In a small part of my projects, I was able to build up some knowledge on MySQL databases.",
+        "übermich-text3" => "Have a look at the register ",  
+        "übermich-text4" => "my projects,",   
+        "übermich-text5" => "there you will find some current written projects.",
+        "datUnterschrift" => "Uznach, 03. September. 2022",
+
+        /* Header titel */
+        "header-meineProjekte" => "my Projects",
+        "header-kontakt" => "Contact",
+        "header-über-mich" => "About me",
+        "header-startseite" => "Home",
+
+    );
+
+?>
